a8e4aaefb57cf6624f5e6750bee1d82714bb731b
anandveeraswamy <anandveeraswamy@gmail.com>
2025-12-03 12:33:28 +0000

    completed step7

--- routes.py
+++ routes.py
@@ -1,14 +1,35 @@
 from app import app
 from flask import render_template
 
+todos = [
+    {
+        "id": 1,
+        "title": "title1",
+        "description": "description1",
+        "created_at": "2025-11-12 06:27:37",
+    },
+    {
+        "id": 2,
+        "title": "title2",
+        "description": "description2",
+        "created_at": "2025-11-12 06:27:37",
+    },
+    {
+        "id": 3,
+        "title": "title3",
+        "description": "description3",
+        "created_at": "2025-11-12 06:27:37",
+    }
+]
+
 @app.route('/') # www.domain.com
 def index():
-    # return "<h1>Todo index page</h1>"
-    return render_template('index.html')
+    todo_count = len(todos)
+    return render_template('index.html', count=todo_count)
 
 @app.route('/tasks') # www.domain.com/tasks
 def all_tasks():
-    return "<h1>List of all tasks</h1>"
+    return render_template('tasks.html')    
 
 @app.route('/task/<int:task_id>') # www.domain.com/task/1
 def task(task_id):
@@ -17,4 +38,4 @@ def task(task_id):
 # www.domain.com/new-task
 @app.route('/new-task')
 def new_task():
-    return "<h1>New task</h1>"
+    return render_template('new_task.html')
