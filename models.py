934557246f16f55d23818ee013d0803246fe0f57
anandveeraswamy <anandveeraswamy@gmail.com>
2025-12-04 12:16:58 +0000

    completed step18

--- models.py
+++ models.py
@@ -3,14 +3,31 @@ import sqlalchemy as sa
 import sqlalchemy.orm as so
 from app import db
 from datetime import datetime
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login
 
+@login.user_loader
+def load_user(id):
+    """ 
+    Flask-Login callback function.
+    converts user id into a User object
+        
+    """
+    return db.session.get(User, int(id))
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     __tablename__ = "users"
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)    
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     todos: so.Mapped[list['Todo']] = so.relationship(back_populates='user')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def get_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f"<User id={self.id} username='{self.username}'>"
--- routes.py
+++ routes.py
@@ -1,7 +1,12 @@
-from app import app
+from app import app, db
 from flask import render_template, request, redirect, url_for, flash
 from datetime import datetime
 from forms import LoginForm
+from models import User, Todo
+from flask_login import current_user, login_user, logout_user, login_required
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from urllib.parse import urlsplit
 
 todos = [
     {
@@ -27,17 +32,20 @@ todos = [
 
 
 @app.route("/")
+@login_required
 def index():
     todo_count = len(todos)
     return render_template("index.html", todo_count=todo_count)
 
 
 @app.route("/tasks")
+@login_required
 def all_tasks():    
     return render_template("tasks.html", todos=todos)
 
 
 @app.route("/task/<int:task_id>")
+@login_required
 def task(task_id):
     index = task_id - 1
     task = todos[index]
@@ -45,6 +53,7 @@ def task(task_id):
 
 
 @app.route("/edit-task/<int:task_id>", methods=["GET", "POST"])
+@login_required
 def edit_task(task_id):
     index = task_id - 1
     task = todos[index]    
@@ -59,6 +68,7 @@ def edit_task(task_id):
 
 
 @app.route("/new-task", methods=["GET", "POST"])
+@login_required
 def create_task():    
     if request.method == "POST":
         task_id = todos[-1]["id"] + 1
@@ -74,6 +84,7 @@ def create_task():
     return render_template("task_form.html", task=None)
 
 @app.route("/delete-task/<int:task_id>", methods=["POST"])
+@login_required
 def delete_task(task_id):
     global todos
     todos = [todo for todo in todos if todo["id"] != task_id ]
@@ -81,9 +92,27 @@ def delete_task(task_id):
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))    
     form = LoginForm()    
     if form.validate_on_submit():
-        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-        print('form submitted')
-        return redirect(url_for('index'))
+        user = db.session.scalar(
+            sa.select(User).where(User.username == form.username.data)            
+        )
+        if user is None or not user.get_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)      
+        next_page = request.args.get('next')
+        # To determine if the URL is absolute or relative, 
+        # I parse it with Python's urlsplit() function and then 
+        # check if the netloc component is set or not.
+        if not next_page or urlsplit(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)      
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
